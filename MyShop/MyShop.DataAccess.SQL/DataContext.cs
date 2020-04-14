6028e5af98f58fa1103d8e31c8e34c0e95392790
PRichie <androidandparanoid@gmail.com>
2020-04-13 22:13:32 -0500

    Install EntityFramework, enable SQL Connections

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        //5. Create a constructor to use the connection string
+            //DefaultConnection will force webconfig checkup
+        public DataContext()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        //6. Create a DB Set and pass on your models
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+    }
+}
