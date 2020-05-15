e848b5195213a7c462fa203573512cdbcc3695fe
PRichie <androidandparanoid@gmail.com>
2020-05-14 19:48:44 -0500

    Added Basket

---
+++ MyShop/MyShop.Core/Models/Basket.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    //3. Create your class out of your baseentity
+   public class Basket : BaseEntity
+    {
+        //4. make a virtual collection, important for entity framework, it will tell the program to load all items from the basket "lazy loading"
+
+        public virtual ICollection<BasketItem> BasketItems { get; set; }
+
+        public Basket()
+        {
+            this.BasketItems = new List<BasketItem>();
+        }
+
+    }
+}
--- MyShop/MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -21,5 +21,10 @@ namespace MyShop.DataAccess.SQL
         //6. Create a DB Set and pass on your models
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+
+        public DbSet<Basket> Baskets { get; set; }
+
+        public DbSet<BasketItem> BasketItems { get; set; }
+
     }
 }
